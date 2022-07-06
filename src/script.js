83ea8302509c36389de440287638f91361162731
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-07-06 22:55:15 +0300

    added functionality to divide the total amount among different number of people

--- src/script.js
+++ src/script.js
@@ -1,6 +1,33 @@
 let billCalculator = document.getElementById('form');
+let totalBill = document.getElementById('total-bill')
+let tip = document.getElementById('tip-percent')
+let billInfo = document.getElementById('bill-text')
+let tipInfo = document.getElementById('tip-text')
+let dividedBill = document.getElementById('bill-individual')
+let peopleCount = document.getElementById('people-count')
+let currency = document.getElementById('currency')
+
+
 billCalculator.addEventListener('submit',(e)=>{
     e.preventDefault()
-    alert('hello')
-    form.reset()
+   calculateTotalBill()
+   form.reset()
 })
+
+function calculateTotalBill(){
+    let bill = parseInt(totalBill.value)
+    let tipAmount =parseInt( (tip.value/100)*bill)
+    let totalAmount = bill + tipAmount
+    let dividedAmount =Math.ceil(totalAmount/parseInt(peopleCount.value))
+    billInfo.innerText = totalAmount
+    tipInfo.innerText = tipAmount
+    dividedBill.innerText = dividedAmount
+    setTimeout(reset,5000)
+
+}
+function reset(){
+    billInfo.innerText = ''
+    tipInfo.innerText = ''
+    dividedBill.innerText = ''
+}
+
